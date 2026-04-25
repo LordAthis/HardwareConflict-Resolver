16402bb38a8345fbe16894eeafc75e8b78dd491f
LordAthis <lordathis@gmail.com>
2026-04-25 19:00:36 +0200

    Update Install-Drivers.ps1

--- Fix/Install-Drivers.ps1
+++ Fix/Install-Drivers.ps1
@@ -1,55 +1,40 @@
-# --- KONFIGURACIO ---
 $PSScriptRoot = Split-Path -Parent $MyInvocation.MyCommand.Definition
-$LocalLog = Join-Path $PSScriptRoot "..\LOG\Install.log"
-$GlobalLog = "C:\Temp\HardwareConflict\LOG\Install.log"
+$LogFile = ".\LOG\Install.log"
+$DriverDir = Join-Path (Split-Path $PSScriptRoot -Parent) "Drivers"
 
-# Fuggveny a driver ellenorzesere (Registry alapjan)
-function Test-DriverInstalled {
-    param($SearchString)
-    $regPath = "HKLM:\SYSTEM\CurrentControlSet\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\*"
-    $drivers = Get-ItemProperty $regPath -ErrorAction SilentlyContinue | Where-Object { $_.DriverDesc -like "*$SearchString*" }
-    return ($null -ne $drivers)
+function Write-AutoLog($msg) {
+    $txt = "$(Get-Date -Format 'HH:mm:ss') - $msg"
+    Write-Host $txt -ForegroundColor Cyan
+    $txt | Out-File $LogFile -Append
 }
 
-function Write-ServiceLog {
-    param($msg)
-    $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
-    "$timestamp - $msg" | Out-File -FilePath $LocalLog -Append
-    "$timestamp - $msg" | Out-File -FilePath $GlobalLog -Append
-    Write-Host $msg
+Write-AutoLog "--- AUTOMATA TELEPITES INDITVA ---"
+
+# 1. Intel HD 4000 Telepitese (Csendes modban)
+$IntelExe = Join-Path $DriverDir "Intel_HD4000.exe"
+if (Test-Path $IntelExe) {
+    Write-AutoLog "Intel Driver telepitese folyamatban..."
+    Start-Process -FilePath $IntelExe -ArgumentList "-s", "-norestart" -Wait
+    Write-AutoLog "Intel Driver KESZ."
 }
 
-# --- VEGREHAJTAS ---
-Write-ServiceLog "--- Intelligens Telepites Ellenorzes Inditasa ---"
+# 2. Intel VGA aktivalasa
+Get-PnpDevice -FriendlyName "*Intel(R) HD Graphics 4000*" | Enable-PnpDevice -Confirm:$false -ErrorAction SilentlyContinue
 
-# 1. Beviteli eszkozok (Billentyuzet, Eger) - EZEKET AZONNAL VISSZA
-Write-ServiceLog "Beviteli eszkozok kenyszeritene visszakapcsolasa (Whitelist)..."
-Get-PnpDevice -FriendlyName "*Keyboard*", "*HID-compliant*", "*Mouse*", "*Touchpad*" | Enable-PnpDevice -Confirm:$false -ErrorAction SilentlyContinue
-
-# 2. Intel HD 4000 Ellenorzese
-$isIntelReady = Test-DriverInstalled "Intel(R) HD Graphics 4000"
-if ($isIntelReady) {
-    Write-ServiceLog "[OK] Intel Driver detektalva. VGA aktivalasa..."
-    Get-PnpDevice -FriendlyName "*Intel(R) HD Graphics 4000*" | Enable-PnpDevice -Confirm:$false
-} else {
-    Write-ServiceLog "[!] Intel Driver HIANYZIK. VGA tiltva marad a fagyas elkerulese veget!"
+# 3. AMD Radeon Telepitese (Csendes modban)
+# A Lenovo AMD drivere altalaban a -s vagy /s kapcsoloval fut csendben
+$AMDExe = Join-Path $DriverDir "AMD_Radeon.exe"
+if (Test-Path $AMDExe) {
+    Write-AutoLog "AMD Driver telepitese folyamatban..."
+    Start-Process -FilePath $AMDExe -ArgumentList "/s", "/v/qn" -Wait
+    Write-AutoLog "AMD Driver KESZ."
 }
 
-# 3. AMD Radeon Ellenorzese (Csak ha az Intel mar kesz)
-if ($isIntelReady) {
-    $isAMDReady = Test-DriverInstalled "Radeon"
-    if ($isAMDReady) {
-        Write-ServiceLog "[OK] AMD Driver detektalva. Kartya aktivalasa..."
-        Get-PnpDevice -FriendlyName "*AMD*" | Enable-PnpDevice -Confirm:$false
-    } else {
-        Write-ServiceLog "[!] AMD Driver HIANYZIK. AMD tiltva marad!"
-    }
-}
+# 4. AMD aktivalasa
+Get-PnpDevice -FriendlyName "*AMD*", "*Radeon*" | Enable-PnpDevice -Confirm:$false -ErrorAction SilentlyContinue
 
-# 4. Egyeb eszkozok (Hang, Wifi, stb.) - Csak ha a VGA-k mar stabilak
-if ($isIntelReady -and $isAMDReady) {
-    Write-ServiceLog "Minden VGA stabil, maradek eszkozok aktivalasa..."
-    Get-PnpDevice | Where-Object { $_.Status -eq "Disabled" } | Enable-PnpDevice -Confirm:$false
-}
+# 5. MINDEN maradek letiltott eszkoz visszakapcsolasa (Hang, Wifi, stb.)
+Write-AutoLog "Osszes maradek hardver visszakapcsolasa..."
+Get-PnpDevice | Where-Object { $_.Status -eq "Disabled" } | Enable-PnpDevice -Confirm:$false -ErrorAction SilentlyContinue
 
-Write-ServiceLog "--- Telepitesi fazis vege ---"
+Write-AutoLog "--- AUTOMATA FOLYAMAT VEGE ---"
