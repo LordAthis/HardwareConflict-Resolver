9cea24dd29600e82ea84c176524e8470935c1846
LordAthis <lordathis@gmail.com>
2026-04-28 01:16:58 +0200

    Clean up Set-NormalBoot.ps1 script
    Removed comments and unnecessary lines from the script.

--- Scripts/Set-NormalBoot.ps1
+++ Scripts/Set-NormalBoot.ps1
@@ -2,11 +2,6 @@ if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
     Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
     exit
 }
-
-# Safeboot torlese
 bcdedit /deletevalue {current} safeboot 2>$null
-
-# Takaritas: Ha maradt volna ResetSafeBoot feladat, toroljuk
 Unregister-ScheduledTask -TaskName "ResetSafeBoot" -Confirm:$false -ErrorAction SilentlyContinue
-
 Write-Host "A gep legkozelebb NORMAL modban indul." -ForegroundColor Green
