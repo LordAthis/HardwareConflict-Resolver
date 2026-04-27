509b929f99ac9baa7f9fd2ef691793b955224f12
LordAthis <lordathis@gmail.com>
2026-04-28 00:29:22 +0200

    Refactor logging and device disabling logic

--- Fix/LenovoG500-GraphicsConflict.ps1
+++ Fix/LenovoG500-GraphicsConflict.ps1
@@ -1,34 +1,30 @@
 $PSScriptRoot = Split-Path -Parent $MyInvocation.MyCommand.Definition
-$LogFile = Join-Path (Split-Path $PSScriptRoot -Parent) "LOG\Fix_Activity.log"
+$LogFile = "C:\Temp\HardwareConflict_Fix_Activity.log"
 
 function Write-Log($msg) {
     $timestamp = Get-Date -Format "HH:mm:ss"
-    if (!(Test-Path (Split-Path $LogFile))) { New-Item -ItemType Directory -Path (Split-Path $LogFile) -Force }
-    "$timestamp - $msg" | Out-File $LogFile -Append
+    "[$timestamp] $msg" | Out-File $LogFile -Append
     Write-Host $msg -ForegroundColor Cyan
 }
 
 Write-Log "--- JAVITAS INDITVA ---"
 
-# AGRESSZIV WHITELIST - Ezeket TILOS letiltani
 $WhiteList = @("*Keyboard*", "*Mouse*", "*HID*", "*Network*", "*Wireless*", "*Wi-Fi*", "*Ethernet*", "*Touchpad*")
-
-# CSAK A VGA ES AUDIO TILTASA
 $Targets = @("*AMD*", "*Radeon*", "*Conexant*", "*Intel(R) Display Audio*")
+
 foreach ($t in $Targets) {
     Get-PnpDevice -FriendlyName $t -ErrorAction SilentlyContinue | Disable-PnpDevice -Confirm:$false -ErrorAction SilentlyContinue
     Write-Log "Tiltva: $t"
 }
 
-# MARADEK HIBASOK (Whitelist ellenorzessel)
 Get-PnpDevice | Where-Object { $_.Problem -ne "NoError" -or $_.Status -eq "Error" } | ForEach-Object {
     $name = $_.FriendlyName
     $safe = $false
     foreach ($w in $WhiteList) { if ($name -like $w) { $safe = $true } }
     if (!$safe) {
         Disable-PnpDevice -InstanceId $_.InstanceId -Confirm:$false -ErrorAction SilentlyContinue
-        Write-Log "Hiba tiltva: $name"
+        Write-Log "Hiba miatti tiltas: $name"
     }
 }
 
-"--- JAVITAS KESZ ---" | Out-File $LogFile -Append
+Write-Log "--- JAVITAS KESZ ---"
