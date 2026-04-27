79868f83a7734dbbee76f8cf3b8c734397683b4a
LordAthis <lordathis@gmail.com>
2026-04-28 00:35:48 +0200

    Clean up comments in Set-SafeBoot-Networking.ps1
    Removed comments related to the reboot option and restoring normal mode.

--- Scripts/Set-SafeBoot-Networking.ps1
+++ Scripts/Set-SafeBoot-Networking.ps1
@@ -1,4 +1,3 @@
-# Admin jogok kerese
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
     Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
     exit
@@ -6,8 +5,6 @@ if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
 
 Write-Host "--- Csokkentett mod beallitasa halozattal ---" -ForegroundColor Cyan
 
-# 1. Beallitjuk a legkozelebbi inditast Csokkentett modra (Halozattal)
-# A 'network' parameter biztositja az internetet es az alap grafikus feluletet
 bcdedit /set {current} safeboot network
 
 if ($LASTEXITCODE -eq 0) {
@@ -17,12 +14,7 @@ if ($LASTEXITCODE -eq 0) {
     Write-Warning "[!] Hiba tortent a bcdedit futtatasa kozben!"
 }
 
-# 2. Opcionális: Automatikus újraindítás felajánlása
 $choice = Read-Host "Szeretned most ujrainditani a gepet? (I/N)"
 if ($choice -eq "I" -or $choice -eq "i") {
     shutdown /r /t 5 /c "Ujrainditas Csokkentett modba 5 masodpercen belul..."
 }
-
-# --- MEGJEGYZES A VISSZAALLITASHOZ ---
-# Ha vegeztel, a normal modba valo visszatereshez ezt kell futtatni:
-# bcdedit /deletevalue {current} safeboot
